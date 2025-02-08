242e44058245450e8a848d4d1b23aba1a4bb83bf
TheLemniscat <noe.barbier32@gmail.com>
2025-02-08 16:09:21 +0100

    Addin first spec

---
+++ cypress/e2e/tests.cy.ts
@@ -0,0 +1,9 @@
+describe('template spec', () => {
+  it('passes', () => {
+    //cy.get(id, un comoposant html).should("exist").should("not.exists")
+    //cy.contains("back-button").click()
+    //cy.contains("form").type("uzaubzec")
+
+    cy.visit('https://example.cypress.io')
+  })
+})
